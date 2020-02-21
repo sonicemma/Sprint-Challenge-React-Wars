25ed7e87ee72f682dc80b908eff175e2ff66dfb1
emma <sonicjane2020@gmail.com>
2020-02-21 09:19:16 -0800

    Styled elements

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -1,14 +1,31 @@
 import React from "react";
+import styled from 'styled-components'
+
+const DataBackground = styled.div`
+    background-color: grey;
+    opacity: 0.8;
+    width: 40%;
+    margin-left: 30%;
+`
+
+const NewHeader = styled.h2`
+    font-family: 'Press Start 2P', cursive;
+    margin: 1%;
+`
+
+const NewPara = styled.p`
+    font-family: 'Geostar Fill', cursive;
+`
 
 const StarCard = props => {
     console.log(props);
     return (
-        <div className="StarData" key={props.id}>
-            <h2>{props.name}</h2>
-            <p>Mass: {props.mass}</p>
-            <p>Height: {props.height}</p>
+        <DataBackground className="StarData" key={props.id}>
+            <NewHeader>{props.name}</NewHeader>
+            <NewPara>Mass: {props.mass}kg</NewPara>
+            <NewPara>Height: {props.height}cm</NewPara>
 
-        </div>
+        </DataBackground>
     )
 }
 export default StarCard;
